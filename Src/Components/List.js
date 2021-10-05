d7908f73e734e2b7b077046c4d5b555874dc8dc6
Greg <greg_markarian@hotmail.com>
2021-10-05 16:09:15 +0300

    ADD: List

---
+++ Src/Components/List.js
@@ -0,0 +1,23 @@
+import React from "react";
+import cn from "classnames";
+import ListItem from "./ListItem";
+
+function List(props) {
+  return (
+    <div className={cn("pt-4")}>
+      <div className={cn("border-2")}>
+        <p className={cn("p-2")}>{props.text}</p>
+
+        {props.items.map((item, idx) => (
+          <ListItem
+            text={item.text}
+            buttonName={item.buttonName}
+            onClick={item.onClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default List;
