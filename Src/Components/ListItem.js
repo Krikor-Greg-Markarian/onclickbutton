3c88f5ba711bcd1a0edc2dc73a9b734c768e505c
Greg <greg_markarian@hotmail.com>
2021-10-05 16:50:00 +0300

    ADD: classname instead of class

--- Src/Components/ListItem.js
+++ Src/Components/ListItem.js
@@ -5,11 +5,11 @@ import Button from "./Button";
 function ListItem(props) {
   return (
     <div className={cn("border-2 p-2")}>
-      <div class="flex">
-        <div class="flex-shrink">
+      <div className="flex">
+        <div className="flex-shrink">
           <div className={cn("text-sm w-8/12")}>{props.text}</div>
         </div>
-        <div class="flex-shrink">
+        <div className="flex-shrink">
           <div>
             <Button
               nameOfTheButton={props.buttonName}
