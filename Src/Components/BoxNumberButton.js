fa82b6b634c54ff9634f5e48a019e6d4d246d6e9
Greg <greg_markarian@hotmail.com>
2021-10-05 16:47:48 +0300

    DEL: unused imports

--- Src/Components/BoxNumberButton.js
+++ Src/Components/BoxNumberButton.js
@@ -1,11 +1,10 @@
 import React from "react";
 import cn from "classnames";
-import Button from "./Button";
 import AnotherButton from "./AnotherButton";
 
 function BoxNumberButton(props) {
   return (
-    <div className = {cn("border-2 w-3/12 p-2")}>
+    <div className={cn("border-2 w-3/12 p-2 mt-2")}>
       <p className={cn("inline")}>{props.leftNumber}</p>
       <p className={cn("inline pl-40")}>{props.rightNumber}</p>
       <AnotherButton buttonName={props.buttonName} onClick={props.onClick} />
