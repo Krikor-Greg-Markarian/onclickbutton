94045c378833544a146b8847a830af7e8cf0603e
Greg <greg_markarian@hotmail.com>
2021-10-05 16:35:16 +0300

    ADD: pl-2

--- Src/Components/Numbers.js
+++ Src/Components/Numbers.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import cn from 'classnames'
+import React from "react";
+import cn from "classnames";
 
 function Numbers(props) {
-    return (
-        <div>
-            <p className = {cn("inline")}>{props.leftNumber}</p>
-            <p className = {cn("inline pl-40")}>{props.rightNumber}</p>
-        </div>
-    )
+  return (
+    <div>
+      <p className={cn("inline pl-2")}>{props.leftNumber}</p>
+      <p className={cn("inline pl-40")}>{props.rightNumber}</p>
+    </div>
+  );
 }
 
-export default Numbers
+export default Numbers;
