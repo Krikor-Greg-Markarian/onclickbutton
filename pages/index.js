ae1a3c7f2359a0619e7e11e9e3c0649152dc7c26
Greg <greg_markarian@hotmail.com>
2021-10-05 16:08:28 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,80 @@
 import React, { useState } from "react";
 import cn from "classnames";
-import BoxButton from "../Src/Components/BoxButton";
-import TextBox from "../Src/Components/TextBox";
-import ArrayTwoBox from "../Src/Components/ArrayTwoBox";
+import ListItem from "../Src/Components/ListItem";
+import List from "../Src/Components/List";
+import Numbers from "../Src/Components/Numbers";
+import AnotherButton from "../Src/Components/AnotherButton";
+import BigBox from "../Src/Components/BigBox";
+import TextBlueBorder from "../Src/Components/TextBlueBorder";
 
 export default function Home(props) {
-  const [isClicked, setIsClicked] = useState(false);
-
   return (
     <div className={cn("container mx-auto")}>
-      <div className={cn("flex justify-center items-center")}>
-        <BoxButton
-          text={"click me"}
-          isSelected={isClicked}
-          onClick={() => setIsClicked(true)}
+      <div>
+        <ListItem
+          text={
+            "asdaLorem ipsum dolor sit amet consectetur, adipisicing elit.  quas. Quia eum conssdasd"
+          }
         />
       </div>
-      <br></br>
-      <br></br>
-      <div className={cn("flex justify-center items-center")}>
-        <TextBox
-          text={"lorem textasdasdsadsadsadsads"}
-          isSelected={isClicked}
-          onClickTop={() => console.log("99")}
-          onClickBottom={() => console.log("88")}
+      <div>
+        <List
+          text={
+            "asdaLorem ipsum dolor sit amet consectetur, adipisicing elit.  quas. Quia eum conssdasd "
+          }
+          items={[
+            {
+              text: "asdaLorem ipsum dolor sit amet consectetur, adipisicing elit.  quas. Quia eum conssdasd",
+              buttonName: "Another Click",
+              onClick: () => console.log("hello world"),
+            },
+            {
+              text: "asdaLorem ipsum dolor sit amet consectetur, adipisicing elit.  quas. Quia eum conssdasd",
+              buttonName: "123",
+              onClick: () => console.log("123"),
+            },
+            {
+              text: "asdaLorem ipsum dolor sit amet consectetur, adipisicing elit.  quas. Quia eum conssdasd",
+              buttonName: "Good bye",
+              onClick: () => console.log("Good bye"),
+            },
+          ]}
         />
       </div>
-      <br></br>
-      <br></br>
-      <ArrayTwoBox
-        text={
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea cumque fugiat id maxime eos molestiae nemo ad expedita a est laudantium, officiis doloribus quaerat! Unde sapiente placeat maiores adipisci? Ullam?"
-        }
-        cancel={"Cancel"}
-        ok={"Ok"}
-        isSelected={isClicked}
-        topButton={() => console.log("Cancel")}
-        buttomButton={() => console.log("Ok")}
-        listItems={[
-          { text: "123" },
-          { text: "123" },
-          { text: "123" },
-          { text: "123" },
-        ]}
-      />
+
+      <div>
+        <Numbers leftNumber={"1"} rightNumber={"2"} />
+
+        <BigBox
+          items={[
+            {
+              leftNumber: "1",
+              rightNumber: "2",
+              buttonName: "Hello",
+              onClick: () => console.log("hey how are you"),
+            },
+            {
+              leftNumber: "1",
+              rightNumber: "2",
+              buttonName: "heyyyy",
+              onClick: () => console.log("123"),
+            },
+            {
+              leftNumber: "1",
+              rightNumber: "2",
+              buttonName: "ahlennn",
+              onClick: () =>
+                console.log(
+                  "eben 3ammi ent sutti w makhassak i za hadan se3adni fehem ???? aw lae???? "
+                ),
+            },
+          ]}
+        />
+
+        <AnotherButton className={cn("")} buttonName={"Apply"} />
+      </div>
+
+      <TextBlueBorder text={"Click Me"} />
     </div>
   );
 }
