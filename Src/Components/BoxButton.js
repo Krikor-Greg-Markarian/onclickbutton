356d4ef20678314dc7edcfd7ee09abaac48daa4d
Greg <greg_markarian@hotmail.com>
2021-10-05 11:48:33 +0300

    ADD: on click blue border

--- Src/Components/BoxButton.js
+++ Src/Components/BoxButton.js
@@ -4,17 +4,12 @@ import cn from "classnames";
 function BoxButton(props) {
   return (
     <div>
-    <div
-      onClick={props.onClick}
-      className={cn("border-2 p-2", { "border-blue-400": props.isSelected })}
-    >
-      <p>{props.text}</p>
-    </div>
-
-  
-   
-
-
+      <div
+        onClick={props.onClick}
+        className={cn("border-2 p-2", { "border-blue-400": props.isSelected })}
+      >
+        <p>{props.text}</p>
+      </div>
     </div>
   );
 }
